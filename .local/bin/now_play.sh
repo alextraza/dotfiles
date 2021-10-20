550abd540eaee65b46b3f87a5313099802302bc9
Дмитрий Голубев <a1extra3a@gmail.com>
2021-10-20 15:07:42 +0300

    add theme to tmux and chanes to now_play

--- .local/bin/now_play.sh
+++ .local/bin/now_play.sh
@@ -1,7 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-if ps -A | grep ncspot > /dev/null; then
-    artist=`playerctl -p ncspot metadata artist`
-    title=`playerctl -p ncspot metadata title`
-    echo " ${artist} - ${title}"
-fi
+PLAYER="ncspot"
+
+pgrep "$PLAYER" >/dev/null || exit 0
+
+get_player_status() {
+	case "$(playerctl -p "$PLAYER" status || true)" in
+		Playing) printf "⏵" ;;
+		Paused)  printf "⏸" ;;
+		*)
+			# Something went wrong
+			true
+	esac
+}
+
+printf " %s %s - %s" \
+	"$(get_player_status)" \
+	"$(playerctl -p "$PLAYER" metadata artist)" \
+	"$(playerctl -p "$PLAYER" metadata title)"
+
+# or
+# playerctl -p ncspot metadata --format '{{emoji(status)}} {{artist}} - {{title}}'
